9e0a534441926a377e9d8a82afefb59b35637dfd
Hugo Targino <71579799+targinohugo@users.noreply.github.com>
2020-09-24 12:19:17 -0300

    Update CaloriasFulfillmentDialogflow.js

--- CaloriasFulfillmentDialogflow.js
+++ CaloriasFulfillmentDialogflow.js
@@ -32,15 +32,17 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     basal = cal+cal2-cal3-161;
 
   \\eu só consegui executar isso, colocando o código abaixo em uma linha só, então tive que usar \n para pular as linhas
-  agent.add("Sua taxa metabólica basal é de" + " " + parseInt(basal) + " " + "calorias" + "  \n" + "  \n" + 
-  "Além da taxa metabólica basal, podemos considerar outros fatores, como por exemplo o nível de atividade." + "  \n" + "  \n" + 
-  "Veja a classificação do fator de atividade que calculamos utilizando seus dados como base:" + "  \n" + "  \n" + 
-  "Nível sedentatário:" + " " + parseInt(basal) + "x1,2" + " " + "=" + " " + parseInt(basal*1.2) + " " + "calorias" + "  \n" + 
-  "Exercícios de 1 a 3 dias na semana:" + " " + parseInt(basal) + "x1,375" + " " + "="  + " " + parseInt(basal*1.375) + " " + "calorias" + "  \n" +
-  "Exercícios de 3 a 5 dias na semana:" + " " + parseInt(basal) + "x1,5" +  " " + "=" + " " + parseInt(basal*1.5) + " " + "calorias" + "  \n" + 
-  "Exercícios de 6 a 7 dias na semana:" + " " + parseInt(basal) + "x1,75" + " " + "=" + " " + parseInt(basal*1.725) + " " + "calorias" + "  \n" + 
-  "Atletas:" + " " + parseInt(basal) + "x1,9" + " " + "=" + " " + parseInt(basal*1.9) + " " + "calorias" + "  \n"  + "  \n" +
-  "Caso você queria mais detalhes sobre como o cálculo foi realizado, digite 'detalhes' ");  
+  agent.add("Sua taxa metabólica basal é de" + " " + parseInt(basal) + " " + "calorias" + " " + "  \n" +
+            "Essa é, em média, a quantidade de calorias que seu corpo gasta simplesmente para sobreviver." + "  \n" + "  \n" +
+            "Porém além da taxa metabólica basal, podemos considerar outros fatores, como por exemplo o nível de atividade." + "  \n" + "  \n" +
+            "Dá uma olhada na tabela abaixo, e veja em qual nível você se classifica:" + "  \n" + "  \n" + 
+            "Nível 1: Sedentários:" + " " + parseInt(basal) + "x1,2" + " " + "=" + " " + parseInt(basal*1.2) + " " + "calorias" + "  \n" +
+            "Nível 2: Exercícios de 1 a 3 dias na semana:" + " " + parseInt(basal) + "x1,375" + " " + "="  + " " + parseInt(basal*1.375) + " " + "calorias" + "  \n" + 
+            "Nível 3: Exercícios de 3 a 5 dias na semana:" + " " + parseInt(basal) + "x1,5" +  " " + "=" + " " + parseInt(basal*1.5) + " " + "calorias" + "  \n" + 
+            "Nível 4: Exercícios de 6 a 7 dias na semana:" + " " + parseInt(basal) + "x1,75" + " " + "=" + " " + parseInt(basal*1.725) + " " + "calorias" + "  \n" + 
+            "Nível 5: Atletas:" + " " + parseInt(basal) + "x1,9" + " " + "=" + " " + parseInt(basal*1.9) + " " + "calorias" + "  \n"  + "  \n" +
+            "Caso você queira mais informações sobre como o cálculo foi realizado, digite 'detalhes' ");  
+
     
     
   }
